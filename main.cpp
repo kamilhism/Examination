f6e5c86efe08c7f9227454d3b812185763881229
kamil <kamil@acer.(none)>
2012-06-22 00:38:24 +0400

    File for example

--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@
 //#include "vector.h"
 //#include "matrix.h"
 //#include "ackermann.h"
-#include "a.b.h"
+//#include "a.b.h"
+//#include "skobki.h"
+//#include "max_piece_of_matrix.h"
+//#include "spiski_askar.h"
+#include "saddle_point.h"
 
 using namespace std;
 
